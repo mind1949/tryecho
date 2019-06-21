968411e835486eff5ded5d01e8fc3a7d646ff40c
mind1949 <lianjie1949@gmail.com>
2019-06-21 17:57:24 +0800

    refactor: 使用echoswg

--- controllers/users.go
+++ controllers/users.go
@@ -1,22 +1,14 @@
 package controllers
 
 import (
-	"github.com/labstack/echo"
 	"iserver/models"
-	"net/http"
-	"strconv"
 )
 
-func FindUser(c echo.Context) error {
-	id, err := strconv.Atoi(c.Param("id"))
+func FindUser(req *struct{ ID *int}) (*models.User, error) {
+	u := &models.User{Id: *req.ID}
+	err := u.Find()
 	if err != nil {
-		return err
+		return nil, err
 	}
-	u := &models.User{Id: id}
-	err = u.Find()
-	if err != nil {
-		return err
-	}
-
-	return c.JSON(http.StatusOK, u)
+	return u, nil
 }
--- server.go
+++ server.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"github.com/yb7/echoswg"
 )
 
 func main() {
@@ -14,8 +15,10 @@ func main() {
 	e := echo.New()
 	// 添加中间件
 	e.Use(middleware.Logger())
+	e.Use(validateSession)
 	// 添加路由
-	e.GET("users/:id", controllers.FindUser, validateSession)
+	g := echoswg.NewApiGroup(e, "", "")
+	g.GET("users/:id", controllers.FindUser)
 	// 启动服务器
 	e.Logger.Fatal(e.Start(":1323"))
 }
